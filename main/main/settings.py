a2b585ad7735f2a274dc344a545386021b530928
alekseyrachuk <alexeyrachuk94@gmail.com>
2022-12-05 01:44:08 +0700

    add requirements.txt, add postgres settings, add db dumb

--- main/main/settings.py
+++ main/main/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "rest_api_blog",
+        "USER": "rest_api_blog",
+        "PASSWORD": "rest_api_blog",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
